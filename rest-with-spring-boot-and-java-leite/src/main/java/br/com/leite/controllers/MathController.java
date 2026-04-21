03b92c08902ce995adf6e32c3c7d1b674fb2738f
Diego Assunção <dev.diegocba@gmail.com>
2026-04-21 17:55:49 -0400

    fix: corrige operações matemáticas e validações no controller

--- rest-with-spring-boot-and-java-leite/src/main/java/br/com/leite/controllers/MathController.java
+++ rest-with-spring-boot-and-java-leite/src/main/java/br/com/leite/controllers/MathController.java
@@ -11,28 +11,67 @@ public class MathController {
 
     // http://localhost:8080/math/sum/3/5
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
-    public Double sum(@PathVariable("numberOne") String numberOne,
-                      @PathVariable("numberTwo") String numberTwo
-    ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new UnsupportedMathOperationException("Por favor, insira um valor numérico!");
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    public Double sum(@PathVariable String numberOne,
+                      @PathVariable String numberTwo) {
+
+        Double n1 = parse(numberOne);
+        Double n2 = parse(numberTwo);
+
+        return n1 + n2;
     }
 
-    private Double convertToDouble(String strNumber) throws IllegalAccessException {
+    // http://localhost:8080/math/subtraction/10/5
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(@PathVariable String numberOne,
+                              @PathVariable String numberTwo) {
 
-        if (strNumber == null || strNumber.isEmpty())
+        Double n1 = parse(numberOne);
+        Double n2 = parse(numberTwo);
+
+        return n1 - n2;
+    }
+
+    // http://localhost:8080/math/division/10/5
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(@PathVariable String numberOne,
+                           @PathVariable String numberTwo) {
+
+        Double n1 = parse(numberOne);
+        Double n2 = parse(numberTwo);
+
+        if (n2 == 0) {
+            throw new UnsupportedMathOperationException("Não é possível dividir por zero!");
+        }
+
+        return n1 / n2;
+    }
+
+    // http://localhost:8080/math/multiplication/3/5
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(@PathVariable String numberOne,
+                                 @PathVariable String numberTwo) {
+
+        Double n1 = parse(numberOne);
+        Double n2 = parse(numberTwo);
+
+        return n1 * n2;
+    }
+
+    private Double parse(String strNumber) {
+        if (!isNumeric(strNumber)) {
             throw new UnsupportedMathOperationException("Por favor, insira um valor numérico!");
-        String number = strNumber.replace(",", ".");// R$ 5,00 USD 5.0
+        }
+
+        String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
     }
 
     private boolean isNumeric(String strNumber) {
         if (strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",", ".");// R$ 5,00 USD 5.0
-        return number.matches("[-+]?[0-9]*\\.?");
-    }
 
-    // http:://localhost:8080/math/subtraction/3/5
-    // http:://localhost:8080/math/division/3/5
+        String number = strNumber.replace(",", ".");
+
+        // Aceita números como: 10, -10, 10.5, -10.5
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
 }
